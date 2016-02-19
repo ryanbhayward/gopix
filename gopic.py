979da41e22623ff5c2591b70d7ef3368bba68948
rbh@imac.home <hayward@ualberta.ca>
2016-02-18 19:23:57 -0700

    working on gopic.py

--- gopic.py
+++ gopic.py
@@ -1,23 +1,32 @@
 import sys
 
-CellDiameter = 10
+CellRadius    =  5
+StoneRadius   = 4
+BoardShade    = .8
 
-L = sys.stdin.readlines()
-for k in range(len(L)):
-  L[k] = L[k].rstrip()
+def goPoint(x,y):
+  return str((2*x+1)*CellRadius)+ ' ' + str((2*y+1)*CellRadius)
+
+def getLines():
+  L = sys.stdin.readlines()
+  for k in range(len(L)):
+    L[k] = L[k].rstrip()
+  return L
 
 def printHeader(rows, cols):
   print '%!PS-Adobe-3.0'
   print '%%BoundingBox: 0 0 ' + \
-    str((1+cols)*CellDiameter) + ' ' + \
-    str((1+rows)*CellDiameter)
+    str((1+cols)*2*CellRadius) + ' ' + \
+    str((1+rows)*2*CellRadius)
   print '%%Pages: 0\n%%EndComments'
+  print '/OriginX 0 def\n/OriginY 0 def'
 
+L = getLines()
 for line in L:
   for x in line.strip():
     print x,
   print ''
 
-print L
-
 printHeader(2,2)
+print goPoint(0,1)
+print goPoint(0,0)
